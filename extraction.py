122599852a13551d622deb7174f32aeda97912d6
Nico Hauser <me@tyratox.ch>
2023-02-16 13:51:35 +0100

    Make extraction run on all input files

--- extraction.py
+++ extraction.py
@@ -63,10 +63,12 @@ def main(input: str, output: str, fmt: str):
             f"Output path must end on '{extension}', '{output}' given"
         )
 
+    df = None
+
     # read all input files, parse them and extract all useful data
-    for input_file in input_files:
+    for i, input_file in enumerate(input_files):
         print(f"Reading compressed csv '{input_file}'")
-        df = pd.read_csv(
+        df_in = pd.read_csv(
             input_file,
             # inputs do not contain any headers
             header=None,
@@ -84,86 +86,91 @@ def main(input: str, output: str, fmt: str):
             # files are gzipped
             compression='gzip'
         )
-        # reduce dataframe to required columns
-        df = df[['certificate_base64', 'certificate_chain_base64']]
-        # convert certificate chain to list column
-        df['certificate_chain_base64'] = df['certificate_chain_base64'].apply(
-            lambda chain: chain.split(";"))
 
-        # store copy of original df with whole certificates
-        cert_df = df
-
-        # reduce dataset for testing purposes
-        # df = df.head(100)
-
-        # extract the certificate data and show a loading bar as it might take a minute
-        df: pd.DataFrame = df.progress_apply(
-            map_certificate_row,
-            axis=1,
-            result_type='expand'
-        )
-
-        # drop all columns where all entries are empty / None
-        df = df.dropna(axis=1, how='all')
-
-        # find all rows that are empty
-        empty_row_indices = df.index[df.isna().all(axis=1)].tolist()
-        invalid_certificates = cert_df.iloc[empty_row_indices]
-        # drop all of the empty rows
-        df = df.drop(index=empty_row_indices)
-
-        # check which columns only consist of one single value
-        # (https://stackoverflow.com/a/54405767/2897827)
-        def single_value_cols(df):
-            a = df.to_numpy()  # df.values (pandas<0.24)
-            return (a[0] == a).all(0)
-
-        # array of booleans indicating whether a column only contains a single value
-        is_single_value_col = single_value_cols(df)
-        # list of column names that are single-valued, will be populated in the loop below
-        single_valued_columns: List[str] = []
-        # create a dictionary mapping from column name to value for the columns that
-        # only contain a single value. will be populated in the loop below
-        single_valued: Dict[str, Any] = {}
-
-        # get any row to retrieve that one value
-        first_row = df.iloc[0]
-
-        # fill 'single_valued_columns' and 'single_valued' by iterating over the
-        # set of all column names and the is_single_value_col array
-        for single_value, column_name in zip(is_single_value_col, df.columns):
-            if single_value:
-                single_valued_columns.append(column_name)
-                single_valued[column_name] = first_row[column_name]
-
-        # drop all single-valued columns
-        df = df.drop(single_valued_columns, axis=1)
-
-        # derive output paths for the single-valued entries and the invalid certificate
-        # by appending a suffix to the filename
-        single_valued_output = derive_output(
-            output,
-            "single-valued",
-            extension,
-            ".csv"
-        )
-        invalid_output = derive_output(
-            output,
-            "invalid",
-            extension,
-            ".csv"
-        )
-
-        # store all of the computed data on disk in the given format
-        if fmt == "parquet":
-            df.to_parquet(output, index=False)
+        if i == 0:
+            df = df_in
         else:
-            df.to_csv(output, index=False)
+            df = pd.concat([df, df_in], ignore_index=True)
 
-        # single valued output and invalid certificates are always stored as csv, they are usually small
-        pd.DataFrame([single_valued]).to_csv(single_valued_output, index=False)
-        invalid_certificates.to_csv(invalid_output, index=False)
-        break
+    # reduce dataframe to required columns
+    df = df[['certificate_base64', 'certificate_chain_base64']]
+    # convert certificate chain to list column
+    df['certificate_chain_base64'] = df['certificate_chain_base64'].apply(
+        lambda chain: chain.split(";"))
+
+    # store copy of original df with whole certificates
+    cert_df = df
+
+    # reduce dataset for testing purposes
+    # df = df.head(100)
+
+    # extract the certificate data and show a loading bar as it might take a minute
+    df: pd.DataFrame = df.progress_apply(
+        map_certificate_row,
+        axis=1,
+        result_type='expand'
+    )
+
+    # drop all columns where all entries are empty / None
+    df = df.dropna(axis=1, how='all')
+
+    # find all rows that are empty
+    empty_row_indices = df.index[df.isna().all(axis=1)].tolist()
+    invalid_certificates = cert_df.iloc[empty_row_indices]
+    # drop all of the empty rows
+    df = df.drop(index=empty_row_indices)
+
+    # check which columns only consist of one single value
+    # (https://stackoverflow.com/a/54405767/2897827)
+    def single_value_cols(df):
+        a = df.to_numpy()  # df.values (pandas<0.24)
+        return (a[0] == a).all(0)
+
+    # array of booleans indicating whether a column only contains a single value
+    is_single_value_col = single_value_cols(df)
+    # list of column names that are single-valued, will be populated in the loop below
+    single_valued_columns: List[str] = []
+    # create a dictionary mapping from column name to value for the columns that
+    # only contain a single value. will be populated in the loop below
+    single_valued: Dict[str, Any] = {}
+
+    # get any row to retrieve that one value
+    first_row = df.iloc[0]
+
+    # fill 'single_valued_columns' and 'single_valued' by iterating over the
+    # set of all column names and the is_single_value_col array
+    for single_value, column_name in zip(is_single_value_col, df.columns):
+        if single_value:
+            single_valued_columns.append(column_name)
+            single_valued[column_name] = first_row[column_name]
+
+    # drop all single-valued columns
+    df = df.drop(single_valued_columns, axis=1)
+
+    # derive output paths for the single-valued entries and the invalid certificate
+    # by appending a suffix to the filename
+    single_valued_output = derive_output(
+        output,
+        "single-valued",
+        extension,
+        ".csv"
+    )
+    invalid_output = derive_output(
+        output,
+        "invalid",
+        extension,
+        ".csv"
+    )
+
+    # store all of the computed data on disk in the given format
+    if fmt == "parquet":
+        df.to_parquet(output, index=False)
+    else:
+        df.to_csv(output, index=False)
+
+    # single valued output and invalid certificates are always stored as csv, they are usually small
+    pd.DataFrame([single_valued]).to_csv(single_valued_output, index=False)
+    invalid_certificates.to_csv(invalid_output, index=False)
 
 
 if __name__ == '__main__':
