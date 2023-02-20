c55c647db5392f40c50bd66e492d454991ed01a2
Nico Hauser <me@tyratox.ch>
2023-02-20 14:26:47 +0100

    Print number of certificates without BC

--- analysis/basic_constraints.py
+++ analysis/basic_constraints.py
@@ -17,3 +17,11 @@ def count_ca_enabled_certs(df: pd.DataFrame):
     print(
         len(df[df['EXTENSION_BASIC_CONSTRAINTS_CA'] == True])
     )
+
+    # print(df[df['id'].isna()]["id"])
+
+    # there are some certificartes without the basic_constraints extension
+    # but this is fine as this results in the flag being set to false
+    # https://www.rfc-editor.org/rfc/rfc5280#section-4.2.1.9
+    # print(df[df['EXTENSION_BASIC_CONSTRAINTS_CA'].isna()]["id"])
+    print(len(df[df['EXTENSION_BASIC_CONSTRAINTS_CA'].isna()]))
