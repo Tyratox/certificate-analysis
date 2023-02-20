bbd0ec39623ca0692967827f6ac62cac710c6013
Nico Hauser <me@tyratox.ch>
2023-02-20 14:26:12 +0100

    Extract ev information

--- extraction_helpers.py
+++ extraction_helpers.py
@@ -101,6 +101,15 @@ extended_key_usage_object_identifier_names = [
     isinstance(getattr(x509.ExtendedKeyUsageOID, a), x509.ObjectIdentifier)
 ]
 
+# obtained from the firefox source code
+# https://hg.mozilla.org/mozilla-central/file/tip/security/certverifier/ExtendedValidation.cpp
+# JSON.stringify([...document.querySelectorAll("span.s")].map(el => el.textContent.slice(1,-1)).filter(txt => (/^([0-9]*\.)+[0-9]*$/).test(txt)))
+ev_identifiers = set([
+    x509.ObjectIdentifier(oid)
+    for oid in ["1.3.6.1.4.1.13769.666.666.666.1.500.9.1", "1.3.6.1.4.1.13769.666.666.666.1.500.9.1", "2.16.756.1.89.1.2.1.1", "2.16.840.1.114404.1.1.2.4.1", "2.16.840.1.114404.1.1.2.4.1", "2.16.840.1.114404.1.1.2.4.1", "1.3.6.1.4.1.6449.1.2.1.5.1", "1.3.6.1.4.1.6449.1.2.1.5.1", "2.16.840.1.114413.1.7.23.3", "2.16.840.1.114413.1.7.23.3", "2.16.840.1.114414.1.7.23.3", "2.16.840.1.114414.1.7.23.3", "2.16.840.1.114412.2.1", "1.3.6.1.4.1.8024.0.2.100.1.2", "2.16.840.1.114028.10.1.2", "2.16.840.1.114028.10.1.2", "2.23.140.1.1", "2.23.140.1.1", "2.16.578.1.26.1.3.3", "1.3.6.1.4.1.34697.2.1", "1.3.6.1.4.1.34697.2.2", "1.3.6.1.4.1.34697.2.3", "1.3.6.1.4.1.34697.2.4", "1.2.616.1.113527.2.5.1.1", "1.2.616.1.113527.2.5.1.1", "1.3.6.1.4.1.14777.6.1.1", "1.3.6.1.4.1.14777.6.1.2", "1.3.6.1.4.1.7879.13.24.1", "1.3.6.1.4.1.40869.1.1.22.3", "1.3.6.1.4.1.4788.2.202.1", "1.3.6.1.4.1.13177.10.1.3.10", "1.3.6.1.4.1.40869.1.1.22.3", "2.16.792.3.0.4.1.1.4", "2.23.140.1.1", "2.23.140.1.1", "1.3.159.1.17.1", "2.16.840.1.114412.2.1",
+                "2.16.840.1.114412.2.1", "2.16.840.1.114412.2.1", "2.16.840.1.114412.2.1", "2.16.840.1.114412.2.1", "2.23.140.1.1", "2.23.140.1.1", "1.3.6.1.4.1.8024.0.2.100.1.2", "1.3.6.1.4.1.6449.1.2.1.5.1", "1.3.6.1.4.1.6449.1.2.1.5.1", "1.3.6.1.4.1.6449.1.2.1.5.1", "2.23.140.1.1", "2.23.140.1.1", "2.16.840.1.114028.10.1.2", "2.16.840.1.114028.10.1.2", "2.16.840.1.114028.10.1.2", "2.16.156.112554.3", "1.2.392.200091.100.721.1", "2.16.756.5.14.7.4.8", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "1.2.156.112559.1.1.6.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1", "2.23.140.1.1"]
+])
+
 
 def map_certificate_extension(name: str, extensions: x509.Extensions, oid: x509.ObjectIdentifier) -> List[Tuple[str, Any]]:
     try:
@@ -185,8 +194,21 @@ def map_certificate_extension(name: str, extensions: x509.Extensions, oid: x509.
             # more PolicyInformation instances.
             # (https://cryptography.io/en/latest/x509/reference/#cryptography.x509.CertificatePolicies)
 
-            # the exact policies are not of interest (yet) but the count might be
-            return [(name + "_COUNT", len(extension._policies))]
+            # check if the certificate is EV
+            is_ca_enabled = False
+            for policy in extension._policies:
+                # print(policy.policy_qualifiers)
+                if policy.policy_identifier in ev_identifiers:
+                    is_ca_enabled = True
+                    break
+                    # print("found ca certificate")
+                    # exit()
+
+            # most policies are not of interest but maybe the count is
+            return [
+                (name + "_COUNT", len(extension._policies)),
+                (name + "_EV", is_ca_enabled),
+            ]
         elif isinstance(extension, x509.AuthorityKeyIdentifier):
             # The authority key identifier extension provides a means of
             # identifying the public key corresponding to the private key used
@@ -417,6 +439,10 @@ def map_certificate_row(row):
         # if map_certificate_name(cert.subject)[names_object_identifier_names.index("COMMON_NAME")] is None:
         #     print(pem_cert)
         #     exit()
+
+        if row['id'] is None:
+            print(row)
+            exit()
 
         return pd.Series(
             [
